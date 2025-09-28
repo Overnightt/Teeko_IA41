6529666ebbe64c6f2df97d6e23a339bea092b170
Adrien <adrien.djouder@gmail.com>
2025-09-28 16:35:39 +0200

    ajout de la fonction check W qui check si un joueur a gagner

--- game.py
+++ game.py
@@ -1,1 +1,34 @@
 #ici on code les règles du jeu et l'interaction avec le joueur
+from plateau import board
+
+#cette fonction detecte si une condition de victoire est remplie et par quel joueur elle a été remplie
+def check_W(board):
+    for i in range(5): 
+        for j in range(5):
+            if board[i][j]==1:
+                if i<2:
+                    if(board[i+1][j]==1 and board[i+2][j]==1 and board[i+3][j]==1):
+                        return 1
+                if j<2:
+                    if (board[i][j+1]==1 and board[i][j+2]==1 and board[i][j+3]==1):
+                        return 1
+                if j<2 and i<2:
+                    if (board[i+1][j+1]==1 and board[i+2][j+2]==1 and board[i+3][j+3]==1):
+                        return 1
+                if j<4 and i<4:
+                    if (board[i+1][j]==1 and board[i+1][j+1]==1 and board[i][j+1]==1):
+                        return 1
+            if board[i][j]==-1:
+                if i<2:
+                    if(board[i+1][j]==-1 and board[i+2][j]==-1 and board[i+3][j]==-1):
+                        return -1
+                if j<2:
+                    if (board[i][j+1]==-1 and board[i][j+2]==-1 and board[i][j+3]==-1):
+                        return -1
+                if j<2 and i<2:
+                    if (board[i+1][j+1]==-1 and board[i+2][j+2]==-1 and board[i+3][j+3]==-1):
+                        return -1
+                if j<4 and i<4:
+                    if (board[i+1][j]==-1 and board[i+1][j+1]==-1 and board[i][j+1]==-1):
+                        return -1
+    return 0
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 #ici on lance le programme
+from game import check_W
+from plateau import board
 
-import plateau
-for row in plateau.board:
-    print(row)
+
+
+board1 = [
+    [-1, 0, 0, 0, 0],
+    [-1, 0, 0, 0, 0],
+    [-1, 0, 0, 0, 0],
+    [-1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+print(check_W(board1))  
