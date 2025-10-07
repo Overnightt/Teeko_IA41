dea146b1eae10b586ee45cc4c5bd98cf7eb7c000
Adrien <adrien.djouder@gmail.com>
2025-10-07 11:18:11 +0200

    debut de la boucle de jeu

--- main.py
+++ main.py
@@ -17,8 +17,34 @@ def print_board(b):
     print()
 
 def jouer_partie():
-    plat0=plateau.board
+    plat0= board
+    dif=int(input("veullez choisir votre dificulté (0:trés facile 1:moyen):"))
     while not check_W(plat0):
+        humain=-1
+        ia=1
+        print_board(plat0)
+        count=0
+        for i in range(5):
+            for j in range(5):
+                if plat0[i][j]==-1:
+                    count+=1
+        if count < 4:
+            x=5
+            y=5
+            while not(0<=x<=4 and 0<=y<=4):
+                print("Ou voulez vous placer votre pion \n")
+                x=int(input("Lignes (0-4) :"))
+                y=int(input("Colonnes (0-4) :"))
+                place_pion(plat0,x,y,humain)
+                if dif == 0:
+                    Minmax_facile(plat0,ia)
+                if dif == 1:
+                    Minmax_moyen(plat0,ia)
+
+
+
+jouer_partie()
+
 
 
 
