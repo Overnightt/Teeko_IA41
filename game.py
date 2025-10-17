cfb118d23c7a360db2a7fbb1cf2ff092a82d1bc7
Adrien <adrien.djouder@gmail.com>
2025-10-17 15:35:44 +0200

    amelioration de la fonction check_W

--- game.py
+++ game.py
@@ -3,33 +3,42 @@
 
 #cette fonction detecte si une condition de victoire est remplie et par quel joueur elle a été remplie
 def check_W(board):
+    directions=[(1,0),(0,1),(1,1),(1,-1)]
     for i in range(5): 
         for j in range(5):
             if board[i][j]==1:
-                if i<2:
-                    if(board[i+1][j]==1 and board[i+2][j]==1 and board[i+3][j]==1):
+                for di,dj in directions:
+                    ni=i+di
+                    nj=j+dj
+                    compte=0
+                    while 0<=ni<=4 and 0<=nj<=4:
+                        if board[ni][nj]==1:
+                            compte+=1
+                            ni+=di
+                            nj+=dj
+                        else:
+                            break
+                    if compte == 3:
                         return 1
-                if j<2:
-                    if (board[i][j+1]==1 and board[i][j+2]==1 and board[i][j+3]==1):
-                        return 1
-                if j<2 and i<2:
-                    if (board[i+1][j+1]==1 and board[i+2][j+2]==1 and board[i+3][j+3]==1):
-                        return 1
-                if j<4 and i<4:
-                    if (board[i+1][j]==1 and board[i+1][j+1]==1 and board[i][j+1]==1):
+                if i <=3 and j<=3:
+                    if board[i+1][j] == 1 and board[i][j+1] == 1 and board[i+1][j+1] == 1:
                         return 1
             if board[i][j]==-1:
-                if i<2:
-                    if(board[i+1][j]==-1 and board[i+2][j]==-1 and board[i+3][j]==-1):
+                for di,dj in directions:
+                    ni=i+di
+                    nj=j+dj
+                    compte=0
+                    while 0<=ni<=4 and 0<=nj<=4:
+                        if board[ni][nj]==-1:
+                            compte+=1
+                            ni+=di
+                            nj+=dj
+                        else:
+                            break
+                    if compte == 3:
                         return -1
-                if j<2:
-                    if (board[i][j+1]==-1 and board[i][j+2]==-1 and board[i][j+3]==-1):
-                        return -1
-                if j<2 and i<2:
-                    if (board[i+1][j+1]==-1 and board[i+2][j+2]==-1 and board[i+3][j+3]==-1):
-                        return -1
-                if j<4 and i<4:
-                    if (board[i+1][j]==-1 and board[i+1][j+1]==-1 and board[i][j+1]==-1):
+                if i <=3 and j<=3:
+                    if board[i+1][j] == -1 and board[i][j+1] == -1 and board[i+1][j+1] == -1:
                         return -1
     return 0
 
